a5234dfa8b1d946f8d834e7efa2bcf27daf2788e
winston <670293316@qq.com>
2017-08-03 01:46:30 -0700

    modify rototessh

--- rototessh.sh
+++ rototessh.sh
@@ -1,1 +1,4 @@
-ssh -gfnNTR x.x.x.x:localport:0.0.0.0:22 -p rotport root@x.x.x.x -o ServerAliveInterval=300
+#vi /etc/ssh/sshd_config
+#GatewayPorts yes
+#service sshd reload
+ssh -gfnNTR 104.224.171.245:12345:0.0.0.0:22 -p 29250 root@104.224.171.245 -o ServerAliveInterval=300
