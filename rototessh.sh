aedd1bf54ecdf66b42ee6ce82ead0c1856013382
winston <670293316@qq.com>
2017-08-03 02:05:16 -0700

    ...

--- rototessh.sh
+++ rototessh.sh
@@ -1,4 +1,4 @@
 #vi /etc/ssh/sshd_config
 #GatewayPorts yes
 #service sshd reload
-ssh -gfnNTR 104.224.171.245:12345:0.0.0.0:22 -p 29250 root@104.224.171.245 -o ServerAliveInterval=300
+ssh -gfnNTR x.x.x.x:12345:0.0.0.0:22 -p xx root@x.x.x.x -o ServerAliveInterval=300
